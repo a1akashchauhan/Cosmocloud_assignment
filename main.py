763d0088be534d6e9ca4d51a702e16c74f38acac
AKASH CHAUHAN <a1akashchauhan@gmail.com>
2024-05-07 01:54:41 +0530

    ok

--- main.py
+++ main.py
@@ -71,13 +71,13 @@ class UpdateStudent(BaseModel):
 
 
 @app.post("/students", status_code=201)
-async def create_student(student: Student):
+def create_student(student: Student):
 
     result = students_collection.insert_one(student.model_dump())
     return {"id": str(result.inserted_id)}
 
 @app.get("/students", status_code=200)
-async def list_students(country: str = Query(None), age: int = Query(None)):
+def list_students(country: str = Query(None), age: int = Query(None)):
 
     query = {}
     if country:
@@ -91,7 +91,7 @@ async def list_students(country: str = Query(None), age: int = Query(None)):
     return {"data": list(students)}
 
 @app.get("/students/{id}", status_code=200)
-async def get_student(id: str):
+def get_student(id: str):
 
     student = students_collection.find_one({"_id": ObjectId(id)}, {"_id": 0})
 
@@ -101,7 +101,7 @@ async def get_student(id: str):
         raise HTTPException(status_code=404, detail="Student not found")
 
 @app.patch("/students/{id}", status_code=204)
-async def update_student(id: str, student: UpdateStudent):
+def update_student(id: str, student: UpdateStudent):
 
     update_fields = {}
     if student.name is not None:
@@ -115,7 +115,7 @@ async def update_student(id: str, student: UpdateStudent):
     students_collection.update_one({"_id": ObjectId(id)}, {"$set": update_fields})
 
 @app.delete("/students/{id}", status_code=200)
-async def delete_student(id: str):
+def delete_student(id: str):
     # Delete student record from MongoDB
     result = students_collection.delete_one({"_id": ObjectId(id)})
     
